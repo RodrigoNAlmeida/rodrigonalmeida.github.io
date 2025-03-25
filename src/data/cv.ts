32f5ddd4d06c431fed0cbf053cbc1a85a315e43e
RodrigoNAlmeida <115704711+RodrigoNAlmeida@users.noreply.github.com>
2025-03-25 09:56:17 +0100

    Changes to news and publications

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -45,29 +45,28 @@ export const skills = [
 
 export const publications = [
 	{
-		title: 'Polyatomic logics and generalized Blok–Esakia theory',
-		authors: 'Rodrigo Nicolau Almeida',
-		journal: 'Journal of Logic and Computation, Volume 34, Issue 5, July 2024, Pages 887–935',
-		time: '2023',
-		link: 'https://academic.oup.com/logcom/advance-article-abstract/doi/10.1093/logcom/exad043/7198104?redirectedFrom=fulltext',
-		abstract: 'This paper presents a novel concept of a polyatomic logic and initiates its systematic study. This approach, inspired by inquisitive semantics, is obtained by taking a variant of a given logic, obtained by looking at the fragment covered by a selector term. We introduce an algebraic semantics for these logics and prove algebraic completeness. These logics are then related to translations, through the introduction of a number of classes of translations involving selector terms, which are noted to be ubiquitous in algebraic logic. In this setting, we also introduce a generalized Blok–Esakia theory, which can be developed for special classes of translations. We conclude by showing some systematic connections between the theory of polyatomic logics and the general Blok–Esakia theory for a wide class of interesting translations.',
-	},
-	{
 		title: 'Π2-rule systems and inductive classes of Gödel algebras',
 		authors: 'Rodrigo Nicolau Almeida',
 		journal: 'Annals of Pure and Applied Logic, Volume 176, Issue 4, April 2025',
 		time: '2025',
 		link: 'https://www.sciencedirect.com/science/article/pii/S0168007225000016?via%3Dihub',
-		abstract: 'In this paper we present a general theory of Π2-rules for systems of intuitionistic and modal logic. We introduce the notions of -rule system and of an inductive class, and provide model-theoretic and algebraic completeness theorems, which serve as our basic tools...',
+		abstract: 'In this paper we present a general theory of Π2-rules for systems of intuitionistic and modal logic. We introduce the notions of -rule system and of an inductive class, and provide model-theoretic and algebraic completeness theorems, which serve as our basic tools.',
 	},
-
+	{
+		title: 'Polyatomic logics and generalized Blok–Esakia theory',
+		authors: 'Rodrigo Nicolau Almeida',
+		journal: 'Journal of Logic and Computation, Volume 34, Issue 5, July 2024, Pages 887–935',
+		time: '2023',
+		link: 'https://academic.oup.com/logcom/advance-article-abstract/doi/10.1093/logcom/exad043/7198104?redirectedFrom=fulltext',
+		abstract: 'This paper presents a novel concept of a polyatomic logic and initiates its systematic study. ',
+	},
 ];
 
 export const accepted_articles = [
 	{
 		title: "Unification with Simple Variable Restrictions and Admissibility of Pi2-rules",
 		authors: "Rodrigo Nicolau Almeida, Silvio Ghilardi",
-		abstract: "We develop a method to recognize admissibility of Π2-rules, relating this problem to a specific instance of the unification problem with linear constants restriction [3], called here unification with simple variable restriction. It is shown that for logical systems enjoying an appropriate algebraic semantics and a finite approximation of left uniform interpolation, this unification with simple variable restriction can be reduced to standard unification. As a corollary, we obtain the decidability of admissibility of Π2-rules for many logical systems.",
+		abstract: "We develop a method to recognize admissibility of Π2-rules, relating this problem to a specific instance of the unification problem with linear constants restriction, called here unification with simple variable restriction. It is shown that for logical systems enjoying an appropriate algebraic semantics and a finite approximation of left uniform interpolation, this unification with simple variable restriction can be reduced to standard unification. As a corollary, we obtain the decidability of admissibility of Π2-rules for many logical systems.",
 		time: "Accepted at AIML 2024 Proceedings",
 		journal: " ",
 		link: "https://rodrigonalmeida.github.io/papers/Unification_with_Simple_Variable_Restrictions_and_Admissibility_of__2.pdf"
@@ -75,7 +74,7 @@ export const accepted_articles = [
 	{
 		title: "A Coalgebraic Semantics for Intuitionistic Modal Logic",
 		authors: "Rodrigo Nicolau Almeida, Nick Bezhanishvili",
-		abstract: "We give a new coalgebraic semantics for intuitionistic modal logic with 2. In particu-lar, we provide a colagebraic representation of intuitionistic descriptive modal framesand of intuitonistic modal Kripke frames based on image-finite posets. This gives asolution to a problem in the area of coalgebaic logic for these classes of frames, raisedexplicitly by Litak (2014) and de Groot and Pattinson (2020). Our key technicaltool is a recent generalization of a construction by Ghilardi, in the form of a rightadjoint to the inclusion of the category of Esakia spaces in the category of Priestleyspaces. As an application of these results, we study bisimulations of intuitionisticmodal frames, describe dual spaces of free modal Heyting algebras, and provide apath towards a theory of coalgebraic intuitionistic logics.",
+		abstract: "We give a new coalgebraic semantics for intuitionistic modal logic. In particular, we provide a coalgebraic representation of intuitionistic descriptive modal frames and of intuitonistic modal Kripke frames based on image-finite posets.",
 		time: "Accepted at AIML 2024 Proceedings",
 		journal: " ",
 		link: "https://rodrigonalmeida.github.io/papers/A_Coalgebraic_Semantics_for_Intuitionistic_Modal_Logic.pdf"
@@ -94,7 +93,7 @@ export const submitted_articles = [
 	{
 		title: "Colimits of Heyting Algebras through Esakia Duality",
 		authors: "Rodrigo Nicolau Almeida",
-		abstract: "In this note we generalize the construction, due to Ghilardi, of the free Heyting algebra generated by a finite distributive lattice, to the case of arbitrary distributive lattices. Categorically, this provides an explicit construction of a left adjoint to the inclusion of Heyting algebras in the category of distributive lattices This is shown to have several applications, both old and new, in the study of Heyting algebras: (1) it allows a more concrete description of colimits of Heyting algebras, as well as, via duality theory, limits of Esakia spaces, by knowing their description over distributive lattices and Priestley spaces; (2) It allows a direct proof of the amalgamation property for Heyting algebras, and of related facts; (3) it allows a proof of the fact that the category of Heyting algebras is co-distributive. We also study some generalizations and variations of this construction to different settings. First, we analyse some subvarieties of Heyting algebras -- such as Boolean algebras, 𝖪𝖢 and 𝖫𝖢 algebras, and show how the construction can be adapted to this setting. Second, we study the relationship between the category of image-finite posets with p-morphisms and the category of posets with monotone maps, showing that a variation of the above ideas provides us with an appropriate general idea. ",
+		abstract: "We generalize the construction, due to Ghilardi, of the free Heyting algebra generated by a finite distributive lattice, to the case of arbitrary distributive lattices. Categorically, this provides an explicit construction of a left adjoint to the inclusion of Heyting algebras in the category of distributive lattices.",
 		time: "Submitted on 12 Feb 2024 (v1), last revised 13 Nov 2024 (this version, v3)",
 		journal: " ",
 		link: "https://arxiv.org/abs/2402.08058"
--- src/data/news.ts
+++ src/data/news.ts
@@ -1,18 +1,7 @@
 export const news = [
     {
-        time: "19 March 2025",
-        content: "This is the first example of news"
+        time: "28 March 2025",
+        content: "I will be visiting the Theory Group in Birmimgham."
     },
-    {
-        time: "20 March 2025",
-        content: "This is the second example of news"
-    },
-    {
-        time: "21 March 2025",
-        content: "This is the third example of news"
-    },
-    {
-        time: "22 March 2025",
-        content: "These news will only be seen on the larger page."
-    }
+   
 ]
