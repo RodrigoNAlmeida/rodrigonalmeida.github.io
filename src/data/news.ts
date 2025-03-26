c420097a8ea598f5d7209c1f502de5b9052e62b3
RodrigoNAlmeida <115704711+RodrigoNAlmeida@users.noreply.github.com>
2025-03-26 11:04:33 +0100

    Update news.ts

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,7 +1,8 @@
 export const news = [
     {
         time: "28 March 2025",
-        content: "I will be visiting the Theory Group in Birmimgham."
+        content: "I will be visiting the <a
+    href="https://staff.fnwi.uva.nl/n.bezhanishvili/" class="font-semibold hover:text-accent"> Theory Group in Birmimgham </a>."
     },
    
 ]
