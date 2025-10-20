7ae930b52c546624edf172becf18ce455996422e
RodrigoNAlmeida <115704711+RodrigoNAlmeida@users.noreply.github.com>
2025-10-20 11:09:46 +0200

    Update news.ts

--- src/data/news.ts
+++ src/data/news.ts
@@ -33,6 +33,7 @@ content: "I offered a project at the ILLC, jointly with [**Simon Lemal**](https:
 {time: "6 May 2025",
 content: "I visited [**Milan University**](https://lac.unimi.it/) where I gave a talk about Higher order uniform interpolation and Pitts problem"
 },
+
 {
         time: "28 March 2025",
         content: "I visited the [**Theory Group in Birmimgham**](https://www.birmingham.ac.uk/research/activity/computer-science/theory-of-computation) where I gave a talk about one step methods for Heyting algebras."
