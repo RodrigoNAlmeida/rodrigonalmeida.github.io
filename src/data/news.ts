954497e3e44c43235563f180dced3231bbc47ec1
Mateus Almeida <m.almeida@vanbreda.nl>
2026-01-15 21:29:02 +0100

    fixed markdown rendering and few missing commas

--- src/data/news.ts
+++ src/data/news.ts
@@ -18,11 +18,11 @@ export const news = [
     },
 	{
         time: "6 April 2025",
-        content: "I visted the <a href="https://lac.unimi.it" class="font-semibold hover:text-accent"> Logic, Algebra and Categories Group in Milan </a>, where I gave a talk on Pitts problem and higher order uniform interpolation."
+        content: "I visted the **[Logic, Algebra and Categories Group in Milan](https://lac.unimi.it)**, where I gave a talk on Pitts problem and higher order uniform interpolation."
     },
     {
         time: "28 March 2025",
-        content: "I visted the <a href="https://www.birmingham.ac.uk/research/activity/computer-science/theory-of-computation" class="font-semibold hover:text-accent"> Theory Group in Birmimgham </a>, where I gave a talk on step by step constructions of Heyting algebras."
+        content: "I visted the **[Theory Group in Birmimgham](https://www.birmingham.ac.uk/research/activity/computer-science/theory-of-computation)**, where I gave a talk on step by step constructions of Heyting algebras."
     },
    
 ]
