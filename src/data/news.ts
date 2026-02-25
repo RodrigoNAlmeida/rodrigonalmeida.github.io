85882b7567460721a730b34cc9e5a87a2d6b937f
RodrigoNAlmeida <115704711+RodrigoNAlmeida@users.noreply.github.com>
2026-02-25 09:08:18 +0100

    Update news.ts

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,28 +1,40 @@
 export const news = [
 
 {
-	time: "27-31 July 2026",
-        content: "TACL 2026 - Krakow, Poland."
+        time: "28 March 2025",
+        content: "I visted the **[Theory Group in Birmimgham](https://www.birmingham.ac.uk/research/activity/computer-science/theory-of-computation)**, where I gave a talk on step by step constructions of Heyting algebras."
     },
+
 {
-	time: "29 June - 3 July 2026",
-        content: "AIML 2026 - Amsterdam, Netherlands."
+        time: "6 April 2025",
+        content: "I visted the **[Logic, Algebra and Categories Group in Milan](https://lac.unimi.it)**, where I gave a talk on Pitts problem and higher order uniform interpolation."
     },
-	{
-	time: "8-12 September 2025",
-        content: "TBILLC 2025 - Kutaisi, Georgia."
-    },
+
 	{
 	time: "21-25 July 2025",
         content: "LATD 2025 - Sienna, Italy."
     },
+
 	{
-        time: "6 April 2025",
-        content: "I visted the **[Logic, Algebra and Categories Group in Milan](https://lac.unimi.it)**, where I gave a talk on Pitts problem and higher order uniform interpolation."
+	time: "8-12 September 2025",
+        content: "TBILLC 2025 - Kutaisi, Georgia."
     },
-    {
-        time: "28 March 2025",
-        content: "I visted the **[Theory Group in Birmimgham](https://www.birmingham.ac.uk/research/activity/computer-science/theory-of-computation)**, where I gave a talk on step by step constructions of Heyting algebras."
+	
+
+{
+	time: "29 June - 3 July 2026",
+        content: "AIML 2026 - Amsterdam, Netherlands."
     },
+    
+{
+	time: "27-31 July 2026",
+        content: "TACL 2026 - Krakow, Poland."
+    },
+
+{
+	time: "29-30 October 2026",
+	content: "Workshop and PhD thesis defence."
+
+},
    
 ]
