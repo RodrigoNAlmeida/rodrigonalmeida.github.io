c97c40c049035304d302fead24e00bfdf0b9cc88
RodrigoNAlmeida <115704711+RodrigoNAlmeida@users.noreply.github.com>
2026-06-21 09:59:23 +0200

    Updated the links

--- src/data/news.ts
+++ src/data/news.ts
@@ -17,23 +17,34 @@ export const news = [
 
 	{
 	time: "8-12 September 2025",
-        content: "TBILLC 2025 - Kutaisi, Georgia."
+        content: "TBILLC 2025 - Kutaisi, Georgia: I presented work on **[regular Heyting algebras](https://github.com/RodrigoNAlmeida/rodrigonalmeida.github.io/tree/main/papers/slides/Tbillc___Regular_Heyting_algebras_and_free_Heyting_extensions_of_Boolean_algebras.pdf)**; Sarah Dukic presented joint work (together with Nick Bezhanishvili) on coalgebraic semantics for Fischer-Servi logic."
+    },
+
+{
+	time: "11-12 April 2026",
+        content: "CMCS 2026 - Turin, Italy: Sarah Dukic presented our joint work on  **[coalgebraic semantics for Fischer-Servi logic](https://github.com/RodrigoNAlmeida/rodrigonalmeida.github.io/tree/main/papers/slides/Tbillc___Regular_Heyting_algebras_and_free_Heyting_extensions_of_Boolean_algebras.pdf)**.
     },
 	
 
 {
 	time: "29 June - 3 July 2026",
-        content: "AIML 2026 - Amsterdam, Netherlands."
+        content: "AIML 2026 - Amsterdam, Netherlands: Matteo De Berardinis will be presenting our joint work on coequivalence relations and descent in modal logic, especially on S5. I will be presenting work on **[uniform local tabularity](https://github.com/RodrigoNAlmeida/rodrigonalmeida.github.io/blob/main/papers/papers/Local_tabularity_and_uniform_local_tabularity.pdf)**, and joint work with **[Simon Lemal](https://github.com/slemal)** and Nick Bezhanishvili **[Nick Bezhanishvili](https://staff.fnwi.uva.nl/n.bezhanishvili/)** on the failure of Craig interpolation for Fischer-Servi logic."
     },
     
 {
 	time: "27-31 July 2026",
-        content: "TACL 2026 - Krakow, Poland."
+        content: "TACL 2026 - Krakow, Poland: I will be presenting joint work with **[Marco Abbadini](https://marcoabbadini-uni.github.io/)** and **[Igor Arrieta](https://sites.google.com/view/igorarrieta/home)** on the **[Heyting-to-topos problem](https://arxiv.org/abs/2606.03861)**. Matteo de Berardinis will be presenting an extended summary of our current research on descent and equivalence relations for non-classical logics. **[Simon Lemal](https://github.com/slemal)** will be presenting joint work (together with Nick Bezhanishvili) on **[superamalgamation for modal lattices](https://arxiv.org/abs/2602.20380)**. Together with **[Qian Chen](https://chenq9901-logic.github.io/)**, we will present our classification of superamalgamation for bi-Gödel algebras."
     },
 
 {
+	time: "25-28 August 2026",
+	content: "Prague -- Workshop on interpolation and amalgamation."
+
+},
+
+{
 	time: "29-30 October 2026",
-	content: "Workshop and PhD thesis defence."
+	content: "Amsterdam -- On the 29th of October I will organize a workshop on the topics of duality theory, algebraic logic and definability problems. On the 30th I will defend my PhD thesis."
 
 },
    
